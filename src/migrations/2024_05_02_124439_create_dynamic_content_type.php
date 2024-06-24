49721330cdc07ac0f2055b59e5a71dbb1090a362
aman <aman5537jains@gmail.com>
2024-06-24 16:21:49 +0530

    changes

--- src/migrations/2024_05_02_124439_create_dynamic_content_type.php
+++ src/migrations/2024_05_02_124439_create_dynamic_content_type.php
@@ -13,7 +13,7 @@ class CreateDynamicContentType extends Migration
      */
     public function up()
     {
-        \DB::statement("CREATE TABLE  `dynamic_content_types` (`id` INT(11) NOT NULL AUTO_INCREMENT , `name` VARCHAR(255) NOT NULL , `configuration` TEXT NOT NULL , `status` ENUM('1','0') NOT NULL , `updated_at` DATETIME NOT NULL , `created_at` DATETIME NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;");
+        \DB::statement("CREATE TABLE  `dynamic_content_types` (`id` INT(11) NOT NULL AUTO_INCREMENT , `slug` VARCHAR(255) NOT NULL , `name` VARCHAR(255) NOT NULL , `configuration` TEXT NOT NULL , `status` ENUM('1','0') NOT NULL , `updated_at` DATETIME NOT NULL , `created_at` DATETIME NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;");
     }
 
     /**
--- src/views/dynamic-view.blade.php
+++ src/views/dynamic-view.blade.php
@@ -8,13 +8,13 @@
     <div class="dashHead-right">
        <div class="dashHead-action">
         @if($controller->hasPermission("add",$module,false) &&  $canAdd)
-        <a class="buttons dbtn-secondary" href="{{$controller->action("create",["view_type"=>"VIEW"])}}"><i class="fas fa-plus-circle"></i>{{'Add '.singularize($module_title)." View"}}</a>
+        <a class="buttons dbtn-secondary" href="{{$controller->action("create",["view_type"=>"VIEW"])}}"><i class="fas fa-plus-circle"></i>{{'Add '.\Illuminate\Support\Str::singular($module_title)." View"}}</a>
       @endif
           @if($controller->hasPermission("add",$module,false) &&  $canAdd)
-            <a class="buttons dbtn-secondary" href="{{$controller->action("create",["view_type"=>"LIST"])}}"><i class="fas fa-plus-circle"></i>{{'Add '.singularize($module_title)." List"}}</a>
+            <a class="buttons dbtn-secondary" href="{{$controller->action("create",["view_type"=>"LIST"])}}"><i class="fas fa-plus-circle"></i>{{'Add '.\Illuminate\Support\Str::singular($module_title)." List"}}</a>
           @endif
           @if($controller->hasPermission("add",$module,false) &&  $canAdd)
-          <a class="buttons dbtn-secondary" href="{{$controller->action("create",["view_type"=>"FORM"])}}"><i class="fas fa-plus-circle"></i>{{'Add '.singularize($module_title)." Form"}}</a>
+          <a class="buttons dbtn-secondary" href="{{$controller->action("create",["view_type"=>"FORM"])}}"><i class="fas fa-plus-circle"></i>{{'Add '.\Illuminate\Support\Str::singular($module_title)." Form"}}</a>
         @endif
        </div>
     </div>
